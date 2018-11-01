bb1ff93876d37be3189d2b388ad3fcd6d94dc1a4
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-11-01 09:26:04 +0300

    ch(setup react router): Setup react router
    - install react-router-dom
    - modify index.js to use routing
    
    [Finishes #161578433]

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
